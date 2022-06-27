43fe6162e027263d594d1c12f532e386db2285d0
Henok Zelalem <henok.zelalem@adey-meselesh.de>
2022-06-27 18:24:03 +0300

    fix

--- contact-us/index.php
+++ contact-us/index.php
@@ -507,13 +507,12 @@ if(!empty($_POST["send"])) {
 	$userName = $_POST["userName"];
   $userEmail = $_POST["userEmail"];
 	$userPhone = $_POST["userPhone"];
-	$userMessage = $_POST["userMessage"];
 	$toEmail = "z.w.henok@gmail.com";
   
 	$mailHeaders = "Name: " . $userName .
 	"\r\n Email: ". $userEmail  . 
 	"\r\n subject: ". $userPhone  . 
-	"\r\n Message: " . $userMessage . "\r\n";
+
 
 	if(mail($toEmail, $userName, $mailHeaders)) {
 	    $message = "Your contact information is received successfully.";
