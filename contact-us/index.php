0b4ffe59343c751c258a216be44e978ab77b22ab
Henok Zelalem <henok.zelalem@adey-meselesh.de>
2022-06-27 18:39:38 +0300

    fix

--- contact-us/index.php
+++ contact-us/index.php
@@ -508,7 +508,7 @@ if(!empty($_POST["send"])) {
   $userEmail = $_POST["userEmail"];
 	$userPhone = $_POST["userPhone"];
 	$userMessage = $_POST["userMessage"];
-	$toEmail = "z.w.henok@gmail.com";
+	$toEmail = "henok.zelalem@berryethiopia.com";
   
 	$mailHeaders = "Name: " . $userName .
 	"\r\n Email: ". $userEmail  . 
@@ -1832,108 +1832,7 @@ if(!empty($_POST["send"])) {
     id="share-link-js"></script>
   <script type="text/javascript" src="../wp-content/plugins/elementor/assets/lib/dialog/dialog.min%EF%B9%96ver=4.9.0.js"
     id="elementor-dialog-js"></script>
-  <script type="text/javascript" id="elementor-frontend-js-before">
-    var elementorFrontendConfig = {
-      environmentMode: {
-        edit: false,
-        wpPreview: false,
-        isScriptDebug: false,
-      },
-      i18n: {
-        shareOnFacebook: "Share on Facebook",
-        shareOnTwitter: "Share on Twitter",
-        pinIt: "Pin it",
-        download: "Download",
-        downloadImage: "Download image",
-        fullscreen: "Fullscreen",
-        zoom: "Zoom",
-        share: "Share",
-        playVideo: "Play Video",
-        previous: "Previous",
-        next: "Next",
-        close: "Close",
-      },
-      is_rtl: false,
-      breakpoints: { xs: 0, sm: 480, md: 768, lg: 1025, xl: 1440, xxl: 1600 },
-      responsive: {
-        breakpoints: {
-          mobile: {
-            label: "Mobile",
-            value: 767,
-            default_value: 767,
-            direction: "max",
-            is_enabled: true,
-          },
-          mobile_extra: {
-            label: "Mobile Extra",
-            value: 880,
-            default_value: 880,
-            direction: "max",
-            is_enabled: false,
-          },
-          tablet: {
-            label: "Tablet",
-            value: 1024,
-            default_value: 1024,
-            direction: "max",
-            is_enabled: true,
-          },
-          tablet_extra: {
-            label: "Tablet Extra",
-            value: 1200,
-            default_value: 1200,
-            direction: "max",
-            is_enabled: false,
-          },
-          laptop: {
-            label: "Laptop",
-            value: 1366,
-            default_value: 1366,
-            direction: "max",
-            is_enabled: false,
-          },
-          widescreen: {
-            label: "Widescreen",
-            value: 2400,
-            default_value: 2400,
-            direction: "min",
-            is_enabled: false,
-          },
-        },
-      },
-      version: "3.5.2",
-      is_static: false,
-      experimentalFeatures: {
-        e_import_export: true,
-        e_hidden_wordpress_widgets: true,
-        "landing-pages": true,
-        "elements-color-picker": true,
-        "favorite-widgets": true,
-        "admin-top-bar": true,
-      },
-      urls: {
-        assets:
-          "https:\/\/hendon.qodeinteractive.com\/wp-content\/plugins\/elementor\/assets\/",
-      },
-      settings: { page: [], editorPreferences: [] },
-      kit: {
-        global_image_lightbox: "yes",
-        active_breakpoints: ["viewport_mobile", "viewport_tablet"],
-        lightbox_enable_counter: "yes",
-        lightbox_enable_fullscreen: "yes",
-        lightbox_enable_zoom: "yes",
-        lightbox_enable_share: "yes",
-        lightbox_title_src: "title",
-        lightbox_description_src: "description",
-      },
-      post: {
-        id: 1239,
-        title: "Contact%20Us%20%E2%80%93%20Hendon",
-        excerpt: "",
-        featuredImage: false,
-      },
-    };
-  </script>
+
   <script type="text/javascript" src="../wp-content/plugins/elementor/assets/js/frontend.min%EF%B9%96ver=3.5.2.js"
     id="elementor-frontend-js"></script>
   <script type="text/javascript"
